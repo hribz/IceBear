42a64d72c4a64c5c2c4433862ed9c5f12533d9e7
hribz <1918256943@qq.com>
2025-01-14 20:06:54 +0800

    remove useless files

--- tmp.py
+++
@@ -1,4 +0,0 @@
-from IncAnalysis.reports_postprocess import postprocess_workspace
-
-postprocess_workspace(workspace="/home/xiaoyu/github/ice-bear-output", this_version="20250112_163102")
-postprocess_workspace(workspace="/home/xiaoyu/github/ice-bear-output", this_version="20250112_171006")
